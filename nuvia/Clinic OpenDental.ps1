715e26da6c318c126270d3e4b4bf11fb141d3240
badsyntaxx <badsyntaxx@gmail.com>
2026-07-02 10:57:01 -0400

    fix bug in get od

--- nuvia/Clinic OpenDental.ps1
+++ nuvia/Clinic OpenDental.ps1
@@ -16,7 +16,7 @@ function odMenu {
             0 { getODVersion }
             1 { getODConfig }
             2 { install22361 }
-            5 { install24341 }
+            7 { install24341 }
             Default { readCommand }
         }
     } catch {
